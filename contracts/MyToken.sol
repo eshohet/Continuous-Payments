dc41adf5fbc9bb87dd7492858515569f1e4931a7
Albert Tran <albertxtran@gmail.com>
2017-12-10 15:45:23 -0800

    First Test

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,19 +2,19 @@ pragma solidity^0.4.2;
 
 contract MyToken {
 
-    mapping (address => uint256) public balanceOf;
+    mapping (address => uint) public balanceOf;
     string public name;
     uint8 public decimals;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint value);
 
-    function MyToken(uint256 initialSupply, string tokenName, uint8 decimalsUnit) {
+    function MyToken(uint initialSupply, string tokenName, uint8 decimalsUnit) {
         balanceOf[msg.sender] = initialSupply;
         name = tokenName;
         decimals = decimalsUnit;
     }
 
-    function transfer(address _to, uint256 _value) {
+    function transfer(address _to, uint _value) {
         require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
         balanceOf[_to] += _value;
         Transfer (msg.sender, _to, _value);
--- test/ContinuousPaymentTest.js
+++ test/ContinuousPaymentTest.js
@@ -1,18 +1,22 @@
-let ContinuousPayment = artifacts.require('./ContinuousPayment.sol');
+let ContinuousPayment = artifacts.require('./ContinuousPayment.sol')
 
 contract('ContinuousPayment', async ([contractor, employer]) => {
-
-    let contract;
+  let contract
 
     beforeEach(async () => {
-        contract = await ContinuousPayment.new(1, {from: contractor});
-        contract.depositPayment({
-            from: employer,
-            value: 3000
-        }); 
-    });
+        contract = await ContinuousPayment.new(web3.toWei(.01, "ether"), { from: contractor })
+            contract.depositPayment({
+                from: employer,
+                value: web3.toWei(1, "ether")
+             }); 
+        });
 
-    it("Test", async () => {
-
-    });
-});
+    it('Test', async () => { 
+       const initialBalance = await web3.eth.getBalance(contractor);
+       await contract.withdrawPayment({
+          from: contractor
+        });
+       const balance = await web3.eth.getBalance(contractor);
+       assert.equal(balance.greaterThan(initialBalance), true, "Final balance should be greater than initial balance");
+    })
+})
